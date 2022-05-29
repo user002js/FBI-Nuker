c52917eed63b9804c28656f6018f35825be69607
user <101312928+user001js@users.noreply.github.com>
2022-05-28 23:15:20 -0300

    Add files via upload

---
+++ FBI-Nuker/src/build-start.bat
@@ -0,0 +1,15 @@
+@echo off
+title Instalando os requerimentos...
+Pause
+rem CONTINUE APENAS SE TIVER O PYTHON INSTALADO E ADICIONADO AO PATH! Pressione qualquer tecla para continuar. . .
+echo on
+pip install -r requirements.txt
+rem Instalando os requirements.txt
+
+cls
+
+@echo off
+title FBI Nuker v3.0 - by user 001 and duda 002
+Pause
+rem Obrigado por usar o FBI Nuker! Pressione qualquer tecla para continuar. . .
+python FBINuker.pyc
